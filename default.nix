ee04e9fef6d25fe36e5b823d1f5901e3bed7ff28
zo-el <joelulahanna@gmail.com>
2019-07-10 00:53:53 -0500

    updated the hash for the hlonix version

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
  holonix-release-tag = "0.0.18";
- holonix-release-sha256 = "1njv2zr9f3kcz5v1p3ff6z4gp49k5l9knh2sss6f71sgmsr5q3zm";
+ holonix-release-sha256 = "0sdsz5xnn7rc170kxqdz98rglp82qjggil67lv0sbbzxdx0rwjgy";
 
  holonix = import (fetchTarball {
   url = "https://github.com/holochain/holonix/tarball/${holonix-release-tag}";
