d0bbfd9b000f6602a48ff9f954f281b28cde8360
zo-el <joelulahanna@gmail.com>
2019-08-08 20:12:30 -0500

    add validation rules for updating revocation keys

--- test/unit_test/revoke_rev_key.js
+++ test/unit_test/revoke_rev_key.js
@@ -5,7 +5,7 @@ const WRONG_SINGED_AUTH_KEY = "D16Dl3Cywos/AS/ANPqsvkRZCCKWPd1KTkdANOxqG1MXRtdCa
 const SIGNED_AUTH_KEY_2 ="LbEReAxFLkkzfOHRBixC7+DYKGao6lPBYsUycVg3NHmNx7p8237/9unBwrt/o+9P4IWkKR+QCYeFxqBNRnn+Dg==";
 const NEW_REVOCATION_KEY = "HcSCJy6gGntzgr95qrpddfqNPGp48mw36pq6m3333pDo8M8kPcmW89TdUM8iwmz";
 const REV_SIGNED_BY_REV_KEY = "1//sUXEZwizqTK6aefikiVQpXQaIbK224woC4DsSJ6U7B2Mx1GO6V9P7Du+rpPiuRVIbPJ9CGfz3vzbftp78AQ==";
-
+const BAD_SIGNED_REV_KEY = "Jkz3AWHO5bEZ11OpsNeotTIr3CGH3wZcyqUAae+xEVy+MwXhrAS1lfzUbWSRQgsSMWUNSjYTtE9NNUHXPkQkBg=="
 async function genesis (liza){
   return await liza.call("dpki", "init",  {params: "{\"revocation_key\": \"HcSCJy6gGntzgr95qrpddfqNPGp48mw36pq6m3333pDo8M8kPcmW89TdUM8iwmz\"}"})
 }
@@ -22,9 +22,13 @@ module.exports = (scenario) => {
     console.log("My Rules: ",my_rules.Ok[0]);
     t.ok(my_rules.Ok[0].entry.revocationKey,REVOCATION_KEY)
 
+// The signature should not match and throw an error
+    const err_on_commit = await liza.call("dpki", "update_rules", {revocation_key:NEW_REVOCATION_KEY, signed_old_revocation_key:BAD_SIGNED_REV_KEY})
+    console.log(err_on_commit);
+    t.ok(err_on_commit.Err )
 
-    const updated_rule_commit = await liza.call("dpki", "update_rules", {revocation_key:NEW_REVOCATION_KEY, signed_old_revocation_key:REV_SIGNED_BY_REV_KEY})
-    t.ok(updated_rule_commit.Ok )
+    const sucessfull_commit = await liza.call("dpki", "update_rules", {revocation_key:NEW_REVOCATION_KEY, signed_old_revocation_key:REV_SIGNED_BY_REV_KEY})
+    t.ok(sucessfull_commit.Ok )
 
     sleep.sleep(5);
 // Check if your getting the right hash
