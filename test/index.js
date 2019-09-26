879c2149a3b5b484779f25c3c8830c91026edb1a
zo-el <joelulahanna@gmail.com>
2019-09-26 18:06:08 -0500

    updated

--- test/index.js
+++ test/index.js
@@ -15,11 +15,19 @@ const orchestrator = new Orchestrator({
   }
 })
 
+const multi_orchestrator = new Orchestrator({
+  middleware: tapeExecutor(require('tape')),
+  globalConfig: {
+    network: 'n3h',
+    logger: false
+  }
+})
+
 require('./unit_test/update_auth_entries')(orchestrator.registerScenario);
 require('./unit_test/set_up_conductor')(orchestrator.registerScenario);
 require('./unit_test/revoke_rev_key')(orchestrator.registerScenario);
 require('./unit_test/test_init')(orchestrator.registerScenario);
-orchestrator.run()
+require('./unit_test/notification')(multi_orchestrator.registerScenario);
 
-require('./unit_test/notification')(orchestrator.registerScenario);
 orchestrator.run()
+multi_orchestrator.run()
