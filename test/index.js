c388a58f14f32fc5f47e85c4fa2871d730e4deec
zo-el <joelulahanna@gmail.com>
2019-09-26 21:18:16 -0500

    added comments

--- test/index.js
+++ test/index.js
@@ -20,6 +20,10 @@ require('./unit_test/set_up_conductor')(orchestrator.registerScenario);
 require('./unit_test/revoke_rev_key')(orchestrator.registerScenario);
 require('./unit_test/test_init')(orchestrator.registerScenario);
 
+// TODO: Issue with the CI PASSING //
+// using the second conductor config in the scenario does not pass in the CI //
+// NOTE that all these tests pass locally. //
+
 // require('./unit_test/notification')(orchestrator.registerScenario);
 
 orchestrator.run()
