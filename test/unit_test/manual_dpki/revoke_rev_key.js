bf951b714ade2da115475748863e7ea6a778276d
zo-el <joelulahanna@gmail.com>
2019-10-01 13:06:00 -0500

    all sleeps

--- test/unit_test/manual_dpki/revoke_rev_key.js
+++ test/unit_test/manual_dpki/revoke_rev_key.js
@@ -1,12 +1,12 @@
 const { simple_conductor_config, handleHack } = require('../../config')
-
+const {sleep}  = require('sleep')
 const REVOCATION_KEY = "HcSCiPdMkst9geux7y7kPoVx3W54Ebwkk6fFWjH9V6oIbqi77H4i9qGXRsDcdbi";
 const NEW_REVOCATION_KEY = "HcSciCmrxP4w5yefdxiSc3W5nY7ic9yzxS4vpeX3iPtnvu7db59FY4z7vj55mDz";
 const REV_SIGNED_BY_REV_KEY = "Aul6kMS4K4rW7wpRiPC154zdDtXRc8ZQEj3wV3eOufOdbBK839045X8SzHoNI0VJVYLVB9YbL6gJ2goja/jxDw==";
 const BAD_SIGNED_REV_KEY = "Jkz3AWHO5bEZ11OpsNeotTIr3CGH3wZcyqUAae+xEVy+MwXhrAS1lfzUbWSRQgsSMWUNSjYTtE9NNUHXPkQkBg=="
 
 async function conductor_init (liza){
-  return await liza.callSync('dpki_happ', "dpki", "init_dpki",  {params: "{\"revocation_key\": \"HcSCiPdMkst9geux7y7kPoVx3W54Ebwkk6fFWjH9V6oIbqi77H4i9qGXRsDcdbi\",\"signed_auth_key\":\"zJkRXrrbvbzbH96SpapO5lDWoElpzB1rDE+4zbo/VthM/mp9qNKaVsGiVKnHkqT4f5J4MGN+q18xP/hwQUKyDA==\"}"})
+  return await liza.call('dpki_happ', "dpki", "init_dpki",  {params: "{\"revocation_key\": \"HcSCiPdMkst9geux7y7kPoVx3W54Ebwkk6fFWjH9V6oIbqi77H4i9qGXRsDcdbi\",\"signed_auth_key\":\"zJkRXrrbvbzbH96SpapO5lDWoElpzB1rDE+4zbo/VthM/mp9qNKaVsGiVKnHkqT4f5J4MGN+q18xP/hwQUKyDA==\"}"})
 }
 
 module.exports = (scenario) => {
@@ -16,22 +16,22 @@ module.exports = (scenario) => {
     await liza.spawn(handleHack)
 
     await conductor_init(liza)
-
+sleep(5)
 // Check if your getting the right hash
-    const my_rules = await liza.callSync('dpki_happ', "dpki", "get_rules", {})
+    const my_rules = await liza.call('dpki_happ', "dpki", "get_rules", {})
     console.log("My Rules: ",my_rules.Ok[0]);
     t.ok(my_rules.Ok[0].entry.revocationKey,REVOCATION_KEY)
-
+sleep(5)
 // The signature should not match and throw an error
-    const err_on_commit = await liza.callSync('dpki_happ', "dpki", "update_rules", {revocation_key:NEW_REVOCATION_KEY, signed_old_revocation_key:BAD_SIGNED_REV_KEY})
+    const err_on_commit = await liza.call('dpki_happ', "dpki", "update_rules", {revocation_key:NEW_REVOCATION_KEY, signed_old_revocation_key:BAD_SIGNED_REV_KEY})
     console.log(err_on_commit);
     t.ok(err_on_commit.Err )
-
-    const sucessfull_commit = await liza.callSync('dpki_happ', "dpki", "update_rules", {revocation_key:NEW_REVOCATION_KEY, signed_old_revocation_key:REV_SIGNED_BY_REV_KEY})
+sleep(5)
+    const sucessfull_commit = await liza.call('dpki_happ', "dpki", "update_rules", {revocation_key:NEW_REVOCATION_KEY, signed_old_revocation_key:REV_SIGNED_BY_REV_KEY})
     t.ok(sucessfull_commit.Ok )
-
+sleep(5)
 // Check if your getting the right hash
-    const my_updated_rules = await liza.callSync('dpki_happ', "dpki", "get_rules", {})
+    const my_updated_rules = await liza.call('dpki_happ', "dpki", "get_rules", {})
     console.log("My Updated Rules: ",my_updated_rules.Ok[0]);
     t.deepEqual(my_updated_rules.Ok[0].entry.revocationKey,NEW_REVOCATION_KEY )
 
--- test/unit_test/manual_dpki/set_up_conductor.js
+++ test/unit_test/manual_dpki/set_up_conductor.js
@@ -1,5 +1,5 @@
 const { simple_conductor_config, handleHack } = require('../../config')
-const sleep  = require('sleep')
+const {sleep}  = require('sleep')
 const REVOCATION_KEY = "HcSCiPdMkst9geux7y7kPoVx3W54Ebwkk6fFWjH9V6oIbqi77H4i9qGXRsDcdbi";
 const SIGNED_AUTH_KEY_1 ="zJkRXrrbvbzbH96SpapO5lDWoElpzB1rDE+4zbo/VthM/mp9qNKaVsGiVKnHkqT4f5J4MGN+q18xP/hwQUKyDA==";
 const WRONG_SINGED_AUTH_KEY = "D16Dl3Cywos/AS/ANPqsvkRZCCKWPd1KTkdANOxqG1MXRtdCaTYYAOO13mcYYtfzWbaagwLk5oFlns2uQneUDg==";
@@ -11,7 +11,7 @@ const SIGNED_AGENT_ENC_KEY_1_BY_REV_KEY = "L0ecyb+IE570ckU1Ln6ODLqv5O2ldXafGX26Q
 const AGENT_SIG_KEY_2 = "HcSCJ6Q45PCMvwdg5rWKjzTVPi9hoo8ixIzSUFh84tNz9hrs8GfP839IiYNT6wi";
 
 async function conductor_init (liza){
-  return await liza.callSync('dpki_happ', "dpki", "init_dpki",  {params: "{\"revocation_key\": \"HcSCiPdMkst9geux7y7kPoVx3W54Ebwkk6fFWjH9V6oIbqi77H4i9qGXRsDcdbi\",\"signed_auth_key\":\"zJkRXrrbvbzbH96SpapO5lDWoElpzB1rDE+4zbo/VthM/mp9qNKaVsGiVKnHkqT4f5J4MGN+q18xP/hwQUKyDA==\"}"})
+  return await liza.call('dpki_happ', "dpki", "init_dpki",  {params: "{\"revocation_key\": \"HcSCiPdMkst9geux7y7kPoVx3W54Ebwkk6fFWjH9V6oIbqi77H4i9qGXRsDcdbi\",\"signed_auth_key\":\"zJkRXrrbvbzbH96SpapO5lDWoElpzB1rDE+4zbo/VthM/mp9qNKaVsGiVKnHkqT4f5J4MGN+q18xP/hwQUKyDA==\"}"})
 }
 
 module.exports = (scenario) => {
@@ -21,35 +21,36 @@ module.exports = (scenario) => {
 
     await liza.spawn(handleHack)
 
-// On conductor_init we have to make this callSync
+// On conductor_init we have to make this call
     let address = await conductor_init(liza)
     t.ok(address)
 
-
+sleep(5)
 // This is to just test out if we get the right keyset_root address
-    const keyset_root_address = await liza.callSync('dpki_happ', "dpki", "get_initialization_data", {})
+    const keyset_root_address = await liza.call('dpki_happ', "dpki", "get_initialization_data", {})
     // add this test when the init is fixed
     // t.equal(keyset_root_address.Ok,address.Ok)
     t.ok(keyset_root_address.Ok)
     console.log("(((((((((((((((((())))))))))))))))))");
     console.log("-------------------->",keyset_root_address);
     console.log("(((((((((((((((((())))))))))))))))))");
+    sleep(5)
 // Check if the key exist for the key
 // This is befor this is created
-    const checking_key_1 = await liza.callSync('dpki_happ', "dpki", "key_status", {key:AGENT_SIG_KEY_1})
+    const checking_key_1 = await liza.call('dpki_happ', "dpki", "key_status", {key:AGENT_SIG_KEY_1})
     t.deepEqual(checking_key_1.Ok,"Doesn't Exists" )
-
+sleep(5)
 // Lets create an agent key
-    const key_commit = await liza.callSync('dpki_happ', "dpki", "create_agent_key", {
+    const key_commit = await liza.call('dpki_happ', "dpki", "create_agent_key", {
       agent_name:"MY_AGENT"
     })
     t.deepEqual(key_commit.Ok,null)
+sleep(5)
 
-
-    const all_keys = await liza.callSync('dpki_happ', "dpki", "get_all_keys", {})
+    const all_keys = await liza.call('dpki_happ', "dpki", "get_all_keys", {})
     console.log(all_keys);
     t.deepEqual(all_keys.Ok.length,2 )
-
+sleep(5)
 
 /*
 Check if the keys exist for the key
@@ -57,15 +58,15 @@ Check if the keys exist for the key
 */
 
   // Checking Agents initial Signing key
-    const checking_key_2 = await liza.callSync('dpki_happ', "dpki", "key_status", {key:AGENT_SIG_KEY_1})
+    const checking_key_2 = await liza.call('dpki_happ', "dpki", "key_status", {key:AGENT_SIG_KEY_1})
     t.deepEqual(checking_key_2.Ok,"live" )
-
+sleep(5)
   // Ceecking Agents initial Encryption key
-    const checking_key_3 = await liza.callSync('dpki_happ', "dpki", "key_status", {key:AGENT_ENC_KEY_1})
+    const checking_key_3 = await liza.call('dpki_happ', "dpki", "key_status", {key:AGENT_ENC_KEY_1})
     t.deepEqual(checking_key_3.Ok,"live" )
-
+sleep(5)
 // Lets Update the keys just created
-    const updated_key = await liza.callSync('dpki_happ', "dpki", "update_key", {
+    const updated_key = await liza.call('dpki_happ', "dpki", "update_key", {
       old_key:AGENT_SIG_KEY_1,
       signed_old_key:SIGNED_AGENT_SIG_KEY_1_BY_REV_KEY,
       context:"NEWAGENT"
@@ -73,18 +74,18 @@ Check if the keys exist for the key
     console.log("Updated Key: ",updated_key);
     t.deepEqual(updated_key.Ok,null)
 
-    sleep.sleep(5)
+sleep(5)
 
 // Check if the key exist for the key
 // Now the old key should be shown as updated and the new should be live
-    const checking_key_4 = await liza.callSync('dpki_happ', "dpki", "key_status", {key:AGENT_SIG_KEY_1})
+    const checking_key_4 = await liza.call('dpki_happ', "dpki", "key_status", {key:AGENT_SIG_KEY_1})
     t.deepEqual(checking_key_4.Ok,"modified" )
-
-    const checking_key_5 = await liza.callSync('dpki_happ', "dpki", "key_status", {key:AGENT_SIG_KEY_2})
+sleep(5)
+    const checking_key_5 = await liza.call('dpki_happ', "dpki", "key_status", {key:AGENT_SIG_KEY_2})
     t.deepEqual(checking_key_5.Ok,"live" )
+sleep(5)
 
-
-    const deleated_key = await liza.callSync('dpki_happ', "dpki", "delete_key", {
+    const deleated_key = await liza.call('dpki_happ', "dpki", "delete_key", {
       old_key:AGENT_ENC_KEY_1,
       signed_old_key:SIGNED_AGENT_ENC_KEY_1_BY_REV_KEY
     })
@@ -92,9 +93,9 @@ Check if the keys exist for the key
     t.equal(deleated_key.Ok,null)
     console.log(" Deleated Key Succesfully ");
 
-    sleep.sleep(5)
+    sleep(5)
 
-    const checking_key_6 = await liza.callSync('dpki_happ', "dpki", "key_status", {key:AGENT_ENC_KEY_1})
+    const checking_key_6 = await liza.call('dpki_happ', "dpki", "key_status", {key:AGENT_ENC_KEY_1})
     t.deepEqual(checking_key_6.Ok,"deleted" )
 
     await liza.kill()
