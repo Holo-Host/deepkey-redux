d4ce0a7256e3832e114b275991d88a207565b085
zo-el <joelulahanna@gmail.com>
2019-10-01 12:17:25 -0500

    added pkgs.nix

---
+++ pkgs.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/31d521ccc5c2b54d057092c552c492d17ceabd50.tar.gz";
+  sha256 = "1mc2ci7xwb7jfg7bz87886mqfwhi12ad6ghp9326s0jy1l7l9ibb";
+})
