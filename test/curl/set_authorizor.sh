4444a62f5ab792e1854ddec876cc479b52674245
zo-el <joelulahanna@gmail.com>
2020-02-20 20:02:00 -0600

    work for testing

--- test/curl/set_authorizor.sh
+++ test/curl/set_authorizor.sh
@@ -6,7 +6,7 @@ echo ""
 echo "Setting Authorizor..."
 echo ""
 
-curl -X POST -H "Content-Type: application/json" -d '{"id": "0", "jsonrpc": "2.0", "method": "call", "params": {"instance_id": "dpki_happ", "zome": "dpki", "function": "set_authorizor", "args": {"authorization_key_path":1,"signed_auth_key":"cjkp7x6lI39sEV5DYBFHtEBqnCjk0khHBtw/tmd6+XD9gsHCDikJTuTJor5NqcxOfapstdT5f3DZVbXjs4xxAQ=="} }}' http://127.0.0.1:8800
+curl -X POST -H "Content-Type: application/json" -d '{"id": "0", "jsonrpc": "2.0", "method": "call", "params": {"instance_id": "dpki_happ", "zome": "dpki", "function": "set_authorizor", "args": {"authorization_key_path":1,"signed_auth_key":"CzFHUTnO0vYLo8FgOSjessHTRgGePLiIjBYqHSI85M1R/wIMFNdg5k8uKNmpBJi5JqvUvofc5LbTu3XZ/UpwCg=="} }}' http://127.0.0.1:9300
 
 echo ""
 echo "========================================================================"
--- zomes/dpki/code/src/key_registration/handlers.rs
+++ zomes/dpki/code/src/key_registration/handlers.rs
@@ -322,8 +322,7 @@ fn derive_key(index: u64, context: &String, key_type: KeyType) -> ZomeApiResult<
         ));
     } else if !list_of_secreats.contains(&agent_seed) {
         hdk::keystore_derive_seed(
-            // NOTE: This should be change device_seed
-            "root_seed".to_string(),
+            "device_seed".to_string(),
             agent_seed.to_owned(),
             context.to_string(),
             index.to_owned(),
