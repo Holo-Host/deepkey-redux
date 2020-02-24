23403fbb0fccf1d4d580d47c67f511f6baa7d605
zo-el <joelulahanna@gmail.com>
2020-02-23 22:30:26 -0600

    updated the nix pkgs to use holonix

--- default.nix
+++ default.nix
@@ -1,24 +1,32 @@
-{ pkgs ? import ./pkgs.nix {}, shell ? false }:
-
-with pkgs;
-
+# This is an example of what downstream consumers of holonix should do
+# This is also used to dogfood as many commands as possible for holonix
+# For example the release process for holonix uses this file
 let
-  inherit (darwin.apple_sdk.frameworks) CoreServices Security;
+
+ # point this to your local config.nix file for this project
+ # example.config.nix shows and documents a lot of the options
+ config = import ./config.nix;
+
+ # START HOLONIX IMPORT BOILERPLATE
+ holonix = import (
+  if ! config.holonix.use-github
+  then config.holonix.local.path
+  else fetchTarball {
+   url = "https://github.com/${config.holonix.github.owner}/${config.holonix.github.repo}/tarball/${config.holonix.github.ref}";
+   sha256 = config.holonix.github.sha256;
+  }
+ ) { config = config; };
+ # END HOLONIX IMPORT BOILERPLATE
+
 in
-
+with holonix.pkgs;
 {
-  deepkey = buildDNA {
-    inherit shell;
+ dev-shell = stdenv.mkDerivation (holonix.shell // {
+  name = "dev-shell";
 
-    name = "deepkey";
-    src = gitignoreSource ./.;
-
-    nativeBuildInputs = [
-      cmake # required by wabt
-      binaryen
-      wasm-gc
-      wabt
-    ]
-    ++ lib.optionals stdenv.isDarwin [ CoreServices ];
-  };
+  buildInputs = [ ]
+   ++ holonix.shell.buildInputs
+   ++ config.buildInputs
+  ;
+ });
 }
--- pkgs.nix
+++
@@ -1,4 +0,0 @@
-import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/aa6826666f94ca70c182f653877fed806fff64c6.tar.gz";
-  sha256 = "1lq7rnsyk2dizysir68p1lf6rsj5w4wkvvm0adj2bh1jnz6ywlhz";
-})
